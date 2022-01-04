d13b6f1abd7b803a8540e7da2147078a70a50c28
S1m <git@sgougeon.fr>
2022-01-04 23:39:45 +0100

    0.2.1

---
+++ get.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION=0.2.1
+
+mkdir -p $VERSION
+cd $VERSION
+wget https://github.com/UP-NextPush/server-app/archive/refs/tags/$VERSION.tar.gz
+tar xf $VERSION.tar.gz
+mv server-app-$VERSION uppush
+tar zcf uppush-$VERSION.tar.gz uppush
+rm -r uppush $VERSION.tar.gz
+openssl dgst -sha512 -sign ~/.nextcloud/certificates/uppush.key uppush-$VERSION.tar.gz | openssl base64 > uppush-$VERSION.tar.gz.sign
